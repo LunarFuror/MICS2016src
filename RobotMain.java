aca261845cfcec72b510efabc0940ca7119e1f31
Grayson Lorenz <lunar_furor@hotmail.com>
2016-04-03 17:44:01 -0500

    Starting overish
    still needs testing for measurements and booping. Getting closer...

--- RobotMain.java
+++ RobotMain.java
@@ -1,9 +1,6 @@
-import java.util.concurrent.atomic.AtomicInteger;
 import lejos.hardware.Button;
-import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
-import lejos.hardware.motor.NXTRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.robotics.chassis.Chassis;
@@ -12,77 +9,78 @@ import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.MovePilot;
 
 public class RobotMain {
+	public enum Phase {
+		CENTER,
+		APPROACH,
+		BOOP
+	}
+	
 	public static void main(String[] args) {
-		final LegoPixy pixy = new LegoPixy(SensorPort.S4);
-		EV3MediumRegulatedMotor booper = new EV3MediumRegulatedMotor(MotorPort.B);
-		EV3MediumRegulatedMotor camPan = new EV3MediumRegulatedMotor(MotorPort.C);
-		booper.setSpeed(booper.getMaxSpeed());
-		
-		Wheel wheel1 = WheeledChassis.modelWheel(new EV3LargeRegulatedMotor(MotorPort.D), 49.5).offset(-65);
-		Wheel wheel2 = WheeledChassis.modelWheel(new EV3LargeRegulatedMotor(MotorPort.A), 49.5).offset(65);
-		Chassis chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, 2);
+		final double CENTER = 140;
+		final double LEFT_DEADZONE = 20;
+		final double RIGHT_DEADZONE = 20;
+		final double ANGLE_FACTOR = 0.289;
+		EV3MediumRegulatedMotor booper = new EV3MediumRegulatedMotor(MotorPort.C);
+		EV3MediumRegulatedMotor camArm = new EV3MediumRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor left = new EV3LargeRegulatedMotor(MotorPort.A);
+		EV3LargeRegulatedMotor right = new EV3LargeRegulatedMotor(MotorPort.D);
+		Wheel wheel1 = WheeledChassis.modelWheel(right, 43.5).offset(-80).invert(true);
+		Wheel wheel2 = WheeledChassis.modelWheel(left, 43.5).offset(80).invert(true);
+		Chassis chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
 		MovePilot pilot = new MovePilot(chassis);
-		pilot.setAngularSpeed(45);
-		final PixyRectangle ball = pixy.getBiggestBlob();
-		//this is supposed to update xy outside of everything
-		Thread th = new Thread(){
-			public void run(){
-				while(!Button.ENTER.isDown()){
-					ball.setRect(pixy.getBiggestBlob());
-					
-					LCD.clearDisplay();
-					LCD.drawString("X :" + ball.x, 0, 1);
-					LCD.drawString("Y :" + ball.y, 0, 2);
-					LCD.drawString("W :" + ball.width, 0, 3);
-					LCD.drawString("H :" + ball.height, 0, 4);
-					LCD.drawString("CX:" + (ball.x + (ball.width/2)), 0, 5);
-					LCD.drawString("CY:" + (ball.y + (ball.height/2)), 0, 6);
-					
-					try {
-						Thread.sleep(200);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-		};
-		//start that shit
-		th.start();
+		LegoPixy pixy = new LegoPixy(SensorPort.S4);
+		Phase p = Phase.CENTER;
 		
-		while (th.isAlive()) {
-			/*
-			//deal with x and y
-			if(x.get()>140){
-				pilot.rotateLeft();
-				while(pilot.isMoving()){
-					if(x.get()>140){}
-					else
-						pilot.stop();
+		booper.rotate(360);		
+		while(Button.ENTER.isUp())
+		{
+			PixyRectangle rec = pixy.getBiggestBlob();
+			//center the ball before moving
+			if(p == Phase.CENTER){
+				if (rec.getCenterX() < CENTER - LEFT_DEADZONE)
+				{
+					double move = (CENTER - rec.getCenterX()) * ANGLE_FACTOR * -1;
+					pilot.rotate(move);
+				}
+				else if (rec.getCenterX() > CENTER + RIGHT_DEADZONE)
+				{
+					double move = (rec.getCenterX() - CENTER) * ANGLE_FACTOR;
+					pilot.rotate(move);
+				}
+				else{
+					p = Phase.APPROACH;
 				}
 			}
-			else if(x.get()<100){
-				pilot.rotateRight();
-				while(pilot.isMoving()){
-				if(x.get()<100){}
-				else
-					pilot.stop();
-				}
+			
+			//ball centered? then approach the ball
+			if(p == Phase.APPROACH){
+				//determine roughly how far away it is now that we're centered
+				//move that distance
+				//check centered
+				//if centered check distance
+				//if close and centered move cam down OR go through process of booping, unsure if we need to move cam honestly.
 			}
-			else if(y.get()<190){
-				pilot.forward();
-				while(pilot.isMoving()){
-					if(y.get()<190){}
-					else
-						pilot.stop();
-				}
+			
+			//Ball centered AND close? Then the cam is down and we need to boop.
+			if(p == Phase.BOOP){
+				//move forward slowly, we can either check this or put a procedure here
+				//boop the ball!
+				//cam up if it was put down
+				//start process over
 			}
-			else{
-				booper.rotate(360);
-			}
-			*/
 		}
-		booper.close();
 		pixy.close();
+		left.close();
+		right.close();
+		
+		try 
+		{
+			Thread.sleep(200);
+		} 
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 }
+
